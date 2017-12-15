a7e7ddf6a885e3891d57dbe939dfd9730feef655
hondsrug <34505563+hondsrug@users.noreply.github.com>
2017-12-15 11:02:25 +0100

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "contact@markups.io";
+        $recipient = "enderfoxplays@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
